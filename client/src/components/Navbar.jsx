28ff7fc454025b7740fd3c5a7a1519a9ccc712a2
vikram17dp <vikramdp505@gmail.com>
2024-10-10 23:29:35 +0530

    small changes related ui

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,19 +1,21 @@
 import React, { useState } from "react";
 import { assets } from "../assets/assets";
 import { NavLink, useNavigate } from "react-router-dom";
+
 const Navbar = () => {
   const navigate = useNavigate();
   const [showMenu, setShowMenu] = useState(false);
   const [token, setToken] = useState(true);
+
   return (
-    <div className="flex items-center justify-between text-sm py-4 mb-5 border-b border-b-gray-500 ">
+    <div className="flex items-center justify-between text-sm pb-2 mb-2  border-b border-b-gray-500">
       <img
         src={assets.logo}
         alt=""
         className="w-44 cursor-pointer"
         onClick={() => navigate("/")}
       />
-      <ul className="hidden md:flex items-start gap-5 font-medium ">
+      <ul className="hidden md:flex items-start gap-5 font-medium">
         <NavLink to={"/"}>
           <li className="py-1">HOME</li>
           <hr className="border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden" />
@@ -34,14 +36,14 @@ const Navbar = () => {
       <div className="flex items-center gap-4">
         {token ? (
           <div className="flex items-center gap-2 group relative cursor-pointer">
-            <img src={assets.profile_pic} className="w-8 rounded-full " alt="" />
+            <img src={assets.profile_pic} className="w-8 rounded-full" alt="" />
             <img src={assets.dropdown_icon} className="w-2.5" alt="" />
             <div className="absolute top-0 right-0 pt-14 text-base font-medium text-gray-600 z-20 hidden group-hover:block">
-                <div className="min-w-48 bg-stone-100 rounded flex flex-col gap-4 p-4">
-                    <p className="hover:text-black cursor-pointer" onClick={()=>navigate('/my-profile')}>My Profile</p>
-                    <p className="hover:text-black cursor-pointer" onClick={()=>navigate('/my-appointments')}>My Appointment</p>
-                    <p className="hover:text-black cursor-pointer" onClick={()=>setToken(false)}>Logout</p>
-                </div>
+              <div className="min-w-48 bg-stone-100 rounded flex flex-col gap-4 p-4">
+                <p className="hover:text-black cursor-pointer" onClick={() => navigate('/my-profile')}>My Profile</p>
+                <p className="hover:text-black cursor-pointer" onClick={() => navigate('/my-appointments')}>My Appointment</p>
+                <p className="hover:text-black cursor-pointer" onClick={() => setToken(false)}>Logout</p>
+              </div>
             </div>
           </div>
         ) : (
