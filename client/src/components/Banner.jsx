088dc9256c62291e76a5d4d4fa286d3ef134a438
vikram17dp <vikramdp505@gmail.com>
2024-10-11 01:31:46 +0530

    small changes for styling

--- client/src/components/Banner.jsx
+++ client/src/components/Banner.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import { assets } from '../assets/assets'
+import { useNavigate } from 'react-router-dom'
 
 const Banner = () => {
+    const navigate = useNavigate()
   return (
     <div className='flex bg-primary rounded-lg px-6 sm:px-10 md:px-14 lg:px-12 my-20 md:mx-10'>
         <div className="flex-1 py-8 sm:py-10 md:py-16 lg:py-24 lg:pl-2 mr-10">
@@ -9,7 +11,7 @@ const Banner = () => {
                 <p className='mr-40'>Book Appointment</p>
                 <p className='mt-4'>With100+Trusted Doctors</p>
             </div>
-            <button className='bg-white text-sm sm:text-base text-gray-600 px-8 py-3 rounded-full mt-6 hover:scale-105 transition-all mr-[26vw]'>create account</button>
+            <button onClick={()=>navigate('/login')} className='bg-white text-sm sm:text-base text-gray-600 px-8 py-3 rounded-full hover:scale-105 transition-all mr-[26vw] mt-10'>create account</button>
         </div>
         <div className='hidden md:block md:w-1/2 lg:w-[370px] relative'>
             <img src={assets.appointment_img} alt="" className='w-full absolute bottom-0 right-0 max-m-md' />
