c1157d8b5d3e247ccd3c42b189b6cbecba9ac88c
vikram17dp <vikramdp505@gmail.com>
2024-10-10 22:02:42 +0530

    basic instaltion for the react and tailwind and assets foalder

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+   <div className='text-red-600'>App</div>
+  )
+}
+
+export default App
