c4e65b90487e5219d6f0c7b384e663c1f8c3d4de
vikram17dp <vikramdp505@gmail.com>
2024-10-10 22:24:54 +0530

    creation of all pages and components to the ui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,32 @@
-import { useState } from 'react'
-
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
+import Home from './pages/Home'
+import Doctors from './pages/Doctors'
+import Login from './pages/Login'
+import Contact from './pages/Contact'
+import About from './pages/About'
+import MyProfile from './pages/MyProfile'
+import MyAppointments from './pages/MyAppointments'
+import Appointment from './pages/Appointment'
+import Navbar from './components/Navbar'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-   <div className='text-red-600'>App</div>
+    <div className="mx-4 sm:mx-[10%]">
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/doctors' element={<Doctors/>}/>
+        <Route path='/doctors/:speciality' element={<Doctors/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/contact' element={<Contact/>}/>
+        <Route path='/my-profile' element={<MyProfile/>}/>
+        <Route path='/ny-appointments' element={<MyAppointments/>}/>
+        <Route path='/appointment/:docId' element={<Appointment/>}/>
+      </Routes>
+    </div>
   )
 }
 
