9bdc3cbd95bac673beb8d53fd676a2c3bca55528
vikram17dp <vikramdp505@gmail.com>
2024-10-10 23:13:25 +0530

    completion of the navbar ui page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -23,7 +23,7 @@ function App() {
         <Route path='/about' element={<About/>}/>
         <Route path='/contact' element={<Contact/>}/>
         <Route path='/my-profile' element={<MyProfile/>}/>
-        <Route path='/ny-appointments' element={<MyAppointments/>}/>
+        <Route path='/my-appointments' element={<MyAppointments/>}/>
         <Route path='/appointment/:docId' element={<Appointment/>}/>
       </Routes>
     </div>
