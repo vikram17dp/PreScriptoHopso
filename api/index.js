aec93c83ae3d923123c87751110fb15a6c5deb37
vikram17dp <vikramdp505@gmail.com>
2024-10-11 23:50:58 +0530

    creating the cloduinary connections

--- api/index.js
+++ api/index.js
@@ -2,10 +2,13 @@ import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
 import connectDB from './config/mongodb.js';
+import connectCloudinary from './config/cloudinary.js';
 
 const app = express();
 
-const port = process.env.PORT || 4000
+const port = process.env.PORT || 4000;
+connectDB();
+connectCloudinary();
 
 app.use(express.json())
 app.use(cors());
@@ -18,4 +21,3 @@ app.listen(port,()=>{
     console.log("server is listing on",port);
     
 })
-connectDB();
