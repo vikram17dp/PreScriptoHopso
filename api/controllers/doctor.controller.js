941950fda0c7b2a54b2cd72efbb8490c33da5505
vikram17dp <vikramdp505@gmail.com>
2024-10-12 11:29:53 +0530

    creating the models and controller

---
+++ api/controllers/doctor.controller.js
@@ -0,0 +1,10 @@
+
+const addDoctor = async(req,res)=>{
+    try {
+        const {name,email,password,speciality,degree,experience,about,fees,address} = req.body;
+    } catch (error) {
+        
+    }
+}
+
+export {addDoctor}
