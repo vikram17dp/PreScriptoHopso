3b5338443ca09ad6edef16e9a60dfb4967656c66
vikram17dp <vikramdp505@gmail.com>
2024-10-12 13:11:30 +0530

    creating and using the authAdmin middleware

--- api/controllers/admin.controller.js
+++ api/controllers/admin.controller.js
@@ -50,7 +50,7 @@ const loginAdmin = async (req,res)=>{
     try {
         const {email,password} = req.body;
         if(email === process.env.ADMIN_EMAIL && password === process.env.ADMIN_PASSWORD){
-            const token =  jwt.sign(email+password,process.env.JWT_TOKEN)
+            const token =  jwt.sign(email+password,process.env.JWT_SCERET)
             res.json({success:true,token})
         }else{
             res.json({success:false,message:"Invalid Credatinals"})
--- api/routes/admin.route.js
+++ api/routes/admin.route.js
@@ -2,10 +2,11 @@ import express from 'express'
 
 import { addDoctor,loginAdmin} from '../controllers/admin.controller.js'
 import upload from '../middlewares/multer.js'
+import authAdmin from '../middlewares/authAdmin.js';
 
 const adminRouter = express.Router();
 
-adminRouter.post('/add-doctor',upload.single('image'),addDoctor)
+adminRouter.post('/add-doctor',authAdmin,upload.single('image'),addDoctor)
 adminRouter.post('/login',loginAdmin);
 
 export default adminRouter;
