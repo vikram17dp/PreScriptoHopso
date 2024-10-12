ec827a5702c27092e1be5715d51fba0627054ded
vikram17dp <vikramdp505@gmail.com>
2024-10-12 23:59:04 +0530

    creating the sidebar page for the Admin

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -7,6 +7,11 @@ import 'react-toastify/dist/ReactToastify.css';
 import { AdminContext } from './context/AdminContext.jsx';
 import Navbar from './components/Navbar.jsx';
 import Sidebar from './components/Sidebar.jsx';
+import { Route, Routes } from 'react-router-dom';
+import DashBoard from './pages/Admin/DashBoard.jsx';
+import AllAppointments from './pages/Admin/AllAppointments.jsx';
+import AddDoctor from './pages/Admin/AddDoctor.jsx';
+import DoctorsLists from './pages/Admin/DoctorsLists.jsx';
 
 function App() {
   const {aToken} = useContext(AdminContext)
@@ -17,6 +22,15 @@ function App() {
       <Navbar/>
       <div className='flex items-start'>
         <Sidebar/>
+        <Routes>
+          <Route path='/' element={<></>}/>
+          <Route path='/admin-dashboard' element={<DashBoard/>}/>
+          <Route path='/all-appointments' element={<AllAppointments/>}/>
+          <Route path='/add-doctor' element={<AddDoctor/>}/>
+          <Route path='/doctor-list' element={<DoctorsLists/>}/>
+
+
+        </Routes>
       </div>
     </div>
   ) : (
---
+++ admin/src/pages/Admin/DoctorsLists.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const DoctorsLists = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default DoctorsLists
