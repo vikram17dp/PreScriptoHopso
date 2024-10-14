bbf0de73af1add296deec97d2439699f97d4c905
vikram17dp <vikramdp505@gmail.com>
2024-10-15 00:09:41 +0530

    small changes realated to the cors

--- api/index.js
+++ api/index.js
@@ -14,10 +14,7 @@ connectCloudinary();
 dotenv.config()
 
 app.use(express.json())
-app.use(cors({
-    origin: 'http://localhost:5173',
-    credentials: true
-}));
+app.use(cors());
 
 app.use('/api/admin',adminRouter)
 app.use('/api/doctor',doctorRouter)
