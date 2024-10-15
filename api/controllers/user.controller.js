f7649de609cf4ef0345950ae1a8ec9b4db1c52c1
vikram17dp <vikramdp505@gmail.com>
2024-10-15 23:43:18 +0530

    creating an api for the updateProfile

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -2,6 +2,7 @@ import validator from "validator";
 import bcrypt from "bcrypt";
 import userModel from "../models/user.model.js";
 import jwt from "jsonwebtoken";
+import {v2 as cloudinary} from 'cloudinary'
 
 export const registeruser = async (req, res) => {
   try {
@@ -117,6 +118,34 @@ export const getProfile = async(req,res)=>{
         const {userId} = req.body;
         const userData = await userModel.findById(userId).select('-password')
         res.json({success:true,userData})
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ success: false, message: error.message });
+    }
+}
+
+export const updateProfile = async(req,res)=>{
+    try {
+        console.log('Request body:', req.body);
+        console.log('Request file:', req.file);
+        
+        const {userId,name,address,phone,dob,gender} = req.body;
+        const imageFile = req.file;
+        if(!name || !address || !phone || !dob || !gender){
+            return res.status(400).json({ success: false, message: "Missing details" });
+        }
+        await userModel.findByIdAndUpdate(userId,{name,phone,dob,gender,address:JSON.parse(address)})
+        
+        
+        if(imageFile){
+            const imageUpload = await cloudinary.uploader.upload(imageFile.path,{resource_type:'image'})
+            const imageURL = imageUpload.secure_url;
+            await userModel.findByIdAndUpdate(userId,{image:imageURL})
+            return res.json({success:true,message:"image updated successfully!"})
+        }
+        
+        res.json({success:true,message:"profile updated"})
+        
     } catch (error) {
         console.error(error);
         return res.status(500).json({ success: false, message: error.message });
--- api/routes/user.routes.js
+++ api/routes/user.routes.js
@@ -1,6 +1,7 @@
 import express from 'express'
-import { getProfile, googleAuth, registeruser, userLogin } from '../controllers/user.controller.js';
+import { getProfile, googleAuth, registeruser, updateProfile, userLogin } from '../controllers/user.controller.js';
 import authUser from '../middlewares/authUser.js';
+import upload from '../middlewares/multer.js';
 
 
 const userRouter = express.Router();
@@ -9,5 +10,6 @@ userRouter.post('/register',registeruser)
 userRouter.post('/login',userLogin)
 userRouter.post('/google',googleAuth)
 userRouter.get('/get-profile',authUser,getProfile)
+userRouter.post('/update-profile',upload.single('image'),authUser,updateProfile)
 
 export default userRouter
