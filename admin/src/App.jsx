c2f3faaf3b2e8ffdf8c8eca16d74751920ea94a4
vikram17dp <vikramdp505@gmail.com>
2024-11-07 21:49:45 +0530

    doctor pages

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -13,6 +13,9 @@ import AllAppointments from './pages/Admin/AllAppointments.jsx';
 import AddDoctor from './pages/Admin/AddDoctor.jsx';
 import DoctorsLists from './pages/Admin/DoctorsLists.jsx';
 import { DoctorContext } from './context/DoctorContext.jsx';
+import DoctorAppointments from './pages/Doctor/DoctorAppointments.jsx';
+import DoctorDashboard from './pages/Doctor/DoctorDashboard.jsx';
+import DoctorProfile from './pages/Doctor/DoctorProfile.jsx';
 
 function App() {
   const {aToken} = useContext(AdminContext)
@@ -24,11 +27,17 @@ function App() {
       <div className='flex items-start'>
         <Sidebar/>
         <Routes>
+          {/* admin routes */}
           <Route path='/' element={<></>}/>
           <Route path='/admin-dashboard' element={<DashBoard/>}/>
           <Route path='/all-appointments' element={<AllAppointments/>}/>
           <Route path='/add-doctor' element={<AddDoctor/>}/>
           <Route path='/doctor-list' element={<DoctorsLists/>}/>
+
+          {/* doctor routes */}
+          <Route path='/doctor-appointments' element={<DoctorAppointments/>}/>
+          <Route path='/doctor-dashboard' element={<DoctorDashboard/>}/>
+          <Route path='/doctor-profile' element={<DoctorProfile/>}/>
 
 
         </Routes>
