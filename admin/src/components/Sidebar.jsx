1a05da799489a95bfd29a88623f6df16b9e8e343
vikram17dp <vikramdp505@gmail.com>
2024-11-08 17:14:46 +0530

    creating an doctor appoinmtents for the doctor page

--- admin/src/components/Sidebar.jsx
+++ admin/src/components/Sidebar.jsx
@@ -20,28 +20,28 @@ const Sidebar = () => {
             to={"/admin-dashboard"}
           >
             <img src={assets.home_icon} alt="" />
-            <p>Dashboard</p>
+            <p className="hidden md:block">Dashboard</p>
           </NavLink>
           <NavLink to={"/all-appointments"} className={({ isActive }) =>
               `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer 
     ${isActive ? "bg-[#F2F3FF] border-r-4 border-primary" : ""}`
             }>
             <img src={assets.appointment_icon} alt="" />
-            <p>Appointments</p>
+            <p className="hidden md:block">Appointments</p>
           </NavLink>
           <NavLink to={"/add-doctor"} className={({ isActive }) =>
               `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer 
     ${isActive ? "bg-[#F2F3FF] border-r-4 border-primary" : ""}`
             }>
             <img src={assets.add_icon} alt="" />
-            <p>Add Doctor</p>
+            <p className="hidden md:block">Add Doctor</p>
           </NavLink>
           <NavLink to={"/doctor-list"} className={({ isActive }) =>
               `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer 
     ${isActive ? "bg-[#F2F3FF] border-r-4 border-primary" : ""}`
             }>
             <img src={assets.people_icon} alt="" />
-            <p>Doctors List</p>
+            <p className="hidden md:block">Doctors List</p>
           </NavLink>
         </ul>
       )}
@@ -55,14 +55,14 @@ const Sidebar = () => {
             to={"/doctor-dashboard"}
           >
             <img src={assets.home_icon} alt="" />
-            <p>Dashboard</p>
+            <p className="hidden md:block">Dashboard</p>
           </NavLink>
           <NavLink to={"/doctor-appointments"} className={({ isActive }) =>
               `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer 
     ${isActive ? "bg-[#F2F3FF] border-r-4 border-primary" : ""}`
             }>
             <img src={assets.appointment_icon} alt="" />
-            <p>Appointments</p>
+            <p className="hidden md:block">Appointments</p>
           </NavLink>
          
           <NavLink to={"/doctor-profile"} className={({ isActive }) =>
@@ -70,7 +70,7 @@ const Sidebar = () => {
     ${isActive ? "bg-[#F2F3FF] border-r-4 border-primary" : ""}`
             }>
             <img src={assets.people_icon} alt="" />
-            <p>Profile</p>
+            <p className="hidden md:block">Profile</p>
           </NavLink>
         </ul>
       )}
--- admin/src/pages/Doctor/DoctorAppointments.jsx
+++ admin/src/pages/Doctor/DoctorAppointments.jsx
@@ -1,17 +1,55 @@
 import React, { useContext, useEffect } from 'react'
 import { DoctorContext } from '../../context/DoctorContext'
+import { AppContext } from '../../context/AppContext'
+import { assets } from '../../assets/assets'
 
 const DoctorAppointments = () => {
     const {appointments,getAllappointments,dToken} = useContext(DoctorContext)
+    const {calculateAge,slotsDateFormat,currency} = useContext(AppContext)
     useEffect(()=>{
         if(dToken){
             getAllappointments()
         }
     },[dToken])
   return (
-    <div>
-      DoctorAppointments
-    </div>
+   
+      <div className='w-full m-5 max-w-6xl'>
+      <p className='mb-3 text-lg font-medium'>All Appointments</p>
+      <div className='bg-white  border rounded text-sm max-h-[80vh] min-h-[60vh] overflow-y-scroll'>
+        <div className='max-sm:hidden sm:grid grid-cols-[0.5fr_2fr_1fr_1fr_3fr_1fr_1fr] grid-flow-col py-3 px-6 border-b'>
+          <p>#</p>
+          <p>Patient</p>
+          <p>Age</p>
+          <p>Date and Time</p>
+          <p>Doctor</p>
+          <p>Fees</p>
+          <p>Action</p>
+        </div>
+        {
+          appointments.map((item,index)=>(
+            <div className='flex flex-wrap justify-between max-sm:gap-5 max-sm:text-base sm:grid grid-cols-[0.5fr_2fr_1fr_1fr_3fr_1fr_1fr] gap-1 items-center text-gray-500 py-3 px-6 border-b hover:bg-gray-100' key={index}>
+                <p className='max-sm:hidden'>{index+1}</p>
+                <div className='flex items-center gap-2'>
+                  <img className='w-8 rounded-full ' src={item.userData.image} alt="image" /> <p >{item.userData.name}</p>
+                </div>
+                <div>
+                <p className='text-xs inline border border-primary px-2 rounded-full'>
+                  {item.payment ? 'Online' :'Cash'}
+                </p>
+                </div>
+                <p className='max-sm:hidden'>{calculateAge(item.userData.dob)}</p>
+                <p>{slotsDateFormat(item.slotDate)},{item.slotTime}</p>
+                <p>{currency}{item.amount}</p>
+                <div className='flex'>
+                  <img className='w-10 cursor-pointer' src={assets.cancel_icon} alt="" />
+                  <img className='w-10 cursor-pointer' src={assets.tick_icon} alt="" />
+                </div>
+            </div>
+          ))
+        }
+        </div>
+        </div>
+
   )
 }
 
